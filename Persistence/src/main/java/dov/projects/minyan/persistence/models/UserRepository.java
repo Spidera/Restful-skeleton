7d75bef4032e988a323506d104cf55a74c4fe532
Warrax81 <dov.laviati@gmail.com>
2015-05-27 18:47:48 +1000

    - Split work into three modules, removed unnecessary dependencies

--- Persistence/src/main/java/dov/projects/minyan/persistence/models/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package dov.projects.minyan.persistence.models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
--- Services/src/main/dov/projects/minyan/services/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package dov.projects.minyan.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
