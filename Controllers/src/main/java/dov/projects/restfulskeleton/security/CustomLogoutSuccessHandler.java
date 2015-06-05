fbb53b8b3677d47bfdddd214f605ac357500dcf7
Dubi Laviati <dov.laviati@gmail.com>
2015-06-05 17:47:38 +1000

    - Added security context and HTTP basic authentication

---
+++ Controllers/src/main/java/dov/projects/restfulskeleton/security/CustomLogoutSuccessHandler.java
@@ -0,0 +1,20 @@
+package dov.projects.restfulskeleton.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Authentication authentication)
+            throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Successfully logged out");
+    }
+}
